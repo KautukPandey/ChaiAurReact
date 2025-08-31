17dd336badb138263aebf7b00e2d725819854a4e
Kautuk <kautukpandey2004@gmail.com>
2025-08-31 18:33:15 +0530

    Routing

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Github() {
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/Kautuk99')
+        .then(response => response.json())
+        .then(data =>{
+            setData(data)
+        })
+    },[])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white text-3xl'>
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/Kautuk99');
+    return response.json()
+}
